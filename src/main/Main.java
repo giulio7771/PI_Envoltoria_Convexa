bd18d74551fff971e7aa7d686786a65f7540dc14
Giulio Giovanella <giuliog@furb.br>
2020-03-19 19:15:24 -0300

    solved with scanner

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,16 +1,9 @@
 package main;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.Scanner;
 
 /**
  *
@@ -18,27 +11,29 @@ import java.util.logging.Logger;
  */
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-        List<Polygon> polygons = new LinkedList<Polygon>();
-        FileReader fr = null;
+    public static void main(String[] args){
+        Scanner scn = null;
         try {
             Path currentRelativePath = Paths.get("");
             String absolutePath = currentRelativePath.toAbsolutePath().toString();
             String path = absolutePath + "/src/main/input.txt";
-            fr = new FileReader(new File(path));
+            scn = new Scanner(new File(path));
             while(true){
                 System.out.println("novo polígono");
-                int lines; 
-                if((lines =  fr.read()) != -1){
-                    int lines_c = (char) lines;
-                    System.out.println("linhas: " + (char) lines_c);
-                    for (int i = 0; i < lines_c; i++) {
-                        int coordinates;
-                        if((coordinates = fr.read()) != -1){
-                            int coordinates_c = (char) coordinates;
-                            System.out.println("coordenates" + coordinates_c);
-                        }
+                Polygon polygon = new Polygon();
+                if(scn.hasNext()){
+                    String lines_str = scn.next().trim();
+                    System.out.println("lines str " + lines_str);
+                    int lines = Integer.parseInt(lines_str);
+                    System.out.println("linhas: " + lines);
+                    for (int i = 0; i < lines; i++) {
+                        int x = Integer.parseInt(scn.next());
+                        int y = Integer.parseInt(scn.next());
+                        System.out.println("ponto (" + x + "," + y + ")");
+                        Point p = new Point(x, y);
+                        polygon.add(p);
                     }
+                    System.out.println(polygon.hasCriticalPoint());
                 }else{
                     break;
                 }
@@ -48,14 +43,5 @@ public class Main {
         }
     }
     
-    boolean CCW(Point p1, Point p2, Point p3)
-    {
-        double value = (p2.getX()-p1.getX()) * (p3.getY()-p1.getY()) 
-                     - (p2.getY()-p1.getY()) * (p3.getX()-p1.getX());
-        if ( value < 0.000001 )
-            return true;
-        else
-            return false;
-    }
     
 }
--- src/main/Polygon.java
+++ src/main/Polygon.java
@@ -14,6 +14,7 @@ import java.util.List;
  */
 public class Polygon {
     private List<Point> points = new LinkedList<Point>();
+
     
     public void add(Point p){
         points.add(p);
@@ -21,6 +22,20 @@ public class Polygon {
     
     public void remove(Point p){
         points.remove(p);
+    }
+    
+    private boolean CCW(Point p1, Point p2, Point p3)
+    {
+        double value = (p2.getX()-p1.getX()) * (p3.getY()-p1.getY()) 
+                     - (p2.getY()-p1.getY()) * (p3.getX()-p1.getX());
+        if ( value < 0.000001 )
+            return true;
+        else
+            return false;
+    }
+
+    boolean hasCriticalPoint() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
